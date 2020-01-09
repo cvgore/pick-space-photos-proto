d6ee28ce2d121b05f2734f7922f12807e0bd21eb
Kacper <kacper@cvgo.re>
2020-01-09 11:41:00 +0100

    Progress

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,16 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
+import { faAppStoreIos } from '@fortawesome/free-brands-svg-icons';
 
-import 'pagepiling.js';
+import 'fullpage.js';
 
-library.add(faUserAstronaut);
+library.add(faUserAstronaut, faAppStoreIos);
 
 dom.watch();
 
 $(document).ready(function() {
-	$('main').pagepiling({
+	$('main').fullpage({
 		navigation: false,
-		scrollingSpeed: 250,
+		scrollingSpeed: 333,
 	});
 });
