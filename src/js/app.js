9ccae3c02e746f0918381d58c78e5d049c61ca0d
Kacper <kacper@cvgo.re>
2020-01-10 21:40:16 +0100

    Add slides content

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,43 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faDesktop, faNetworkWired, faSatellite } from '@fortawesome/free-solid-svg-icons';
 import { faAppStoreIos } from '@fortawesome/free-brands-svg-icons';
 
 import 'fullpage.js';
 
-library.add(faUserAstronaut, faAppStoreIos);
+library.add(faAppStoreIos, faChevronDown, faDesktop, faSatellite, faNetworkWired);
 
 dom.watch();
 
 $(document).ready(function() {
+	const animateLogo = (reverse = false) => {
+		const $logo = $('.logo');
+		const $brand = $('.navbar-brand > img');
+		
+		if (!reverse) {
+			let pxMoveX = Math.abs($logo.offset().left - $brand.offset().left) + 24;
+			let pxMoveY = Math.abs($logo.offset().top - $brand.offset().top) + 5;
+			$logo.css({transform: `translate3d(-${pxMoveX}px, -${pxMoveY}px, 0)`, width: '60px'});
+		} else {
+			$logo.css({transform: '', width: ''});
+		}
+	};
+
 	$('main').fullpage({
 		navigation: false,
-		scrollingSpeed: 333,
+		scrollingSpeed: 500,
+		onLeave: (idx, nextIdx, dir) => {
+			if (idx === 1 && dir === 'down') {
+				animateLogo(false);
+			} else if (idx === 2 && dir === 'up') {
+				animateLogo(true);
+			}
+
+			if (nextIdx === 4 && dir === 'down') {
+				$('.next-slide-down').fadeOut();
+			} else if (idx === 4 && dir === 'up') {
+				$('.next-slide-down').fadeIn();
+			}
+		}
 	});
+
 });
